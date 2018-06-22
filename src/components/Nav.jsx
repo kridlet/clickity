31df4fca0c08479d40df4121a56e3eb8cbbd9645
Dirk Wiggina <d@irk.com>
2018-06-22 13:29:54 -0400

    nav link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -19,7 +19,7 @@ export class Nav extends Component {
         return (
             <nav className="navbar">
                 <ul>
-                    <li className="brand"><a href="/index.html">Clickity-Click-Click</a></li>
+                    <li className="brand"><a href="https://kridlet.github.io/clickity/index.html">Clickity-Click-Click</a></li>
                     <li className={className}>{navMessage}</li>
                     <li>Clicks: {this.props.currentClicks} | Clicks to Beat: {this.props.highClicks}</li>
                 </ul>
